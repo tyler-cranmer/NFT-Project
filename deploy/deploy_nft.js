1b3d1d0da6f73f91d28b65691052e3d52608cbeb
tyler-cranmer <tyler.cranmer@outlook.com>
2021-11-19 13:53:50 -0700

    deployment scripts

--- deploy/deploy_nft.js
+++ deploy/deploy_nft.js
@@ -1,3 +1,4 @@
+const { ethers } = require('hardhat');
 const fs = require('fs');
 let { networkConfig } = require('../helper-hardhat-config');
 
@@ -11,27 +12,32 @@ module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
   const chainId = await getChainId();
 
   log('--------------------------------------');
-  const NFFEET = await deploy(contract_name, {
+  const NFFeet = await deploy(contract_name, {
     // deploys contract
     from: deployer,
     args:['NFFeet', 'Feet', '123456'],
     log: true,
   });
 
-  log(`You deployed a NFT contract to ${NFFEET.address}`);
+  log(`You deployed a NFT contract to ${NFFeet.address}`);
   log(`The deployer address: ${deployer}`)
-  // const NffeetContract = await ethers.getContractFactory(contract_name); //grabs the NFFeet contract factory.
+
+  const factory = await ethers.getContractFactory(contract_name); //grabs the NFFeet contract factory.
   const accounts = await hre.ethers.getSigners(); //grabs an account.
   const signer = accounts[0];
 
+  const tNFTcontract = new ethers.Contract(NFFeet.address, factory.interface, signer )
+
+  log(`This is the accounts entity: ${accounts}`)
+  log(`This is the Signer entity: ${signer}`);
 
   const networkName = networkConfig[chainId]['name'];
   log(
-    `Verify with: \n npx hardhat verify --network ${networkName} ${NFFEET.address}`
+    `Verify with: \n npx hardhat verify --network ${networkName} ${NFFeet.address}`
   );
 
-//   let transactionReponse = await NFfeet.create(svg); //calls the create function
-//   let receipt = await transactionReponse.wait(1); //wait 1 block for transaction to go through.
-//   log('You created an NFT');
-//   log(`you can view the tokenURI here ${await NFfeet.tokenURI(0)}`);
+  let transactionReponse = await tNFTcontract.mint(1); //calls the mint function
+  let receipt = await transactionReponse.wait(1); //wait 1 block for transaction to go through.
+  log('You created an NFT');
+  log(`you can view the tokenURI here ${await tNFTcontract.tokenURI(1)}`);
 };
